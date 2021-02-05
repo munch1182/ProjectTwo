4b2fe37b4836593ee8e094357c25d63e0c265365
munch1182 <munch1182@qq.com>
2021-02-05 14:43:32 +0800

    clear

--- autocallannotation/src/main/java/com/munch/annotation/autocall/AutoCall.kt
+++
@@ -1,5 +0,0 @@
-package com.munch.annotation.autocall
-
-@Retention(AnnotationRetention.SOURCE)
-@Target(AnnotationTarget.FUNCTION)
-annotation class AutoCall(val target: String, val priority: Int = 0)
